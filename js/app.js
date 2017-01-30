292ed0ca085e162302a82a1fdfb52801e5d1e4c7
Eduardo Garcia <eagarcia8@gmail.com>
2017-01-29 21:02:50 -0800

    Fixed API calls, other updates.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,34 @@
 $(document).ready(function() {
 
-  userArray = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", , "brunofin", "comster404", "habathcx", "RobotCaleb", "noobs2ninjas"];
+  userArray = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "brunofin", "comster404", "habathcx", "RobotCaleb", "noobs2ninjas"];
+
+  /*
+
+  $(".userStatus div").on('click',function () {
+      $(".userStatus div").removeClass('element');
+      $(this).addClass('element');
+  });
+
+  $("#statusBar").click(function () {
+      $(".streaming").show();
+  })
+
+  */
+
+  $(".allUsers").click(function () {
+    $(".row .Online").fadeIn();
+    $(".row .Offline").fadeIn();
+  })
+
+  $(".onlineUsers").click(function () {
+    $(".row .Online").fadeIn();
+    $(".row .Offline").fadeOut();
+  })
+
+  $(".offlineUsers").click(function () {
+    $(".row .Offline").fadeIn();
+    $(".row .Online").fadeOut();
+  })
 
   userArray.forEach(function(user) {
 
@@ -10,71 +38,60 @@ $(document).ready(function() {
       data: {id: 123}
     }).then(function(firstResponse) {
 
-      userStatus = firstResponse.stream;
+      var userStatus = new Object();
 
-      // FIX THIS
-
-      if (firstResponse.stream) {
-        userStatus = "Online";
-      }else {
-        userStatus = "Offline";
-      };
-
-      console.log(userStatus);
-
+      userStatus.name = user;
+      userStatus.live = firstResponse.stream;
 
       $.ajax("https://wind-bow.gomix.me/twitch-api/channels/" + user, {
         jsonp: 'callback',
         dataType: 'jsonp',
         data: {id: 123}
       }).then(function(secondResponse) {
-        // EVERYTHING GOES IN HERE
-        userData = secondResponse;
 
-        console.log(user);
+        // food for thought: When userStatus is created in this loop, but the next loop iteration has started and this API call has just finished, does JavaScript know that this userStatus is refering to the version of the previous loop iteration?
+        userStatus.data = secondResponse;
 
-        if(userStatus == "offline") {
-          var url = "https://www.twitch.tv/" + userData.name; //url with the "channel.name" for existing channels
-        }
-        else {
-          url = '#';
-        }
-
-//show all channels
-
-
-        $(".streaming").append(
-          "<div class='row "+ userData.status + "' id='" + user +"'>"+
-          "<div class='col-sm-3'><img class='img-circle' width='150' height='150'" + "src='" + userData.logo +"'></div>"+
-          "<div class='col-sm-3'><p><a target='_blank' href=" + url + ">" + userData.name + "</a></p></div>" +
-           "<div class='col-sm-6'>" + /*innerHTML*/ "#" + "</div></div>"
-        );
-
-
-        $(".userStatus div").on('click',function () {
-            $(".userStatus div").removeClass('element');
-            $(this).addClass('element');
-        });
-
-        $("#statusBar").click(function () {
-            $(".streaming").show();
-        })
-
-        $(".onlineUsers").click(function () {
-            $(".streaming .row").show();
-            $(".streaming .offlineUsers").hide();
-        })
-
-        $(".offlineUsers").click(function () {
-            $(".streaming").show();
-            $(".streaming .onlineUsers").hide();
-        })
-
-        console.log("The Script has finished.");
+        loadHTML(userStatus);
       });
 
     });
 
   });
+
+  function loadHTML(user) {
+
+    var userLogo;
+    var userStatus;
+    var onlineStatus;
+
+    if (user.live == null) {
+      onlineStatus = "Offline";
+    } else {
+      onlineStatus = "Online";
+    }
+
+    if (user.data.status == 404) {
+      userLogo = "<div class='col-sm-4'><img class='img-circle' width='150' height='150'" + "src='http://placehold.it/150x150'></div>";
+      userStatus = "<div class='col-sm-4'>" + user.name + " does not exist or was banhammered!</div>"
+    } else {
+      userLogo = "<div class='col-sm-4'><img class='img-circle' width='150' height='150'" + "src='" + user.data.logo + "'></div>";
+      userStatus = "<div class='col-sm-4'>" + user.name + " is currently " + "<a target='_blank' href=" + "https://www.twitch.tv/" + user.name + ">" + onlineStatus + "!</a>"
+
+      if (onlineStatus == "Online") {
+        userStatus += "<p>" + user.name + " is streaming " + user.live.game + "<p><img src='" + user.live.preview.medium + "' width='320' height='180'>";
+
+        console.log(user.live.preview.medium);
+      }
+
+      userStatus += "</div>";
+    }
+
+    $(".streaming").append(
+      "<div class='row " + onlineStatus + "' id='" + user.name + "'>" + userLogo +
+      "<div class='col-sm-4'><p><a target='_blank' href=" + "https://www.twitch.tv/" + user.name + ">" + user.name + "</a></p></div>" + userStatus + "</div>"
+    );
+
+  };
 
 });
